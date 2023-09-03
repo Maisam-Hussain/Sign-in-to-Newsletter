fd32118547ad132b4f2a1e046105da2816d53e01
Maisam-Hussain <hussainmaisam62@gmail.com>
2023-09-03 04:45:04 -0700

    update the package.json file as well as *.html and app.js files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const app = express();
-
+const port = 5000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -38,6 +38,7 @@ app.post("/", function(req, res)
       }
     ]
   };
+
   const jsonData = JSON.stringify(data);
   const API_KEY = process.env.API_KEY;
 
@@ -48,33 +49,31 @@ app.post("/", function(req, res)
     method: "POST",
     auth: `Maisam:${API_KEY}-us14`
   }
-  const request = https.request(url, options, function(response)
-  {
-  if(response.statusCode === 200)
-    {
-     res.sendFile(__dirname + "/success.html");
+
+  const request = https.request(url, options, function(response){
+
+    if(response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html");
     }
-    else
-    {
-      res.sendFile(__dirname + "/failure.html");
-  }
 
-
-    response.on("data", function(data)
-    {
+    response.on("data", function(data) {
       console.log(JSON.parse(data));
-    })
-  })
+    });
+
+  });
 
   request.write(jsonData);
   request.end();
+
 });
 
-app.post("/failure", function(req,res)
-{
+
+app.post("/failure", function(req,res) {
   res.redirect("/");
 });
 
-app.listen(3000, function(){
-  console.log("Server is running on Port:3000");
+app.listen(process.env.PORT || port, () =>{
+  console.log(`Server is running on Port:${port}`);  
 });
